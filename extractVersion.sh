0f25ea58e7dec8ddf3056e5b0b4fe6558e868efa
doowwood <doowhsalf@me.com>
2024-02-12 00:52:52 +0100

    Commit for Docker Image build

--- extractVersion.sh
+++ extractVersion.sh
@@ -1,7 +1,4 @@
 # a bash script that extract the version number from the package.json file
 # and print it to the console
-echo "Extracting version number from package.json"
 version=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
-echo "Version number is $version"
-echo "##vso[task.setvariable variable=version]$version"
-
+echo $version
